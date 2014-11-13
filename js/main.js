b1038d7260536de7f243c6b29c88ef7ef04d5c68
Haakon <hravikster@gmail.com>
2014-11-13 03:53:41 +0100

    Added map functionality: Queries the API for coordinates of level-4 facilities and adds markers. Work in progress.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,42 @@
 var app = angular.module('facilityRegistry',[]);
 
-// The controller, used by the <body> tag in the main.html file.
-// We probably no longer need this.
-app.controller('SearchController', function($scope){
-
-});
-
 app.controller('TestController', ['$http', function($http) {
+
 	var testCtrl = this;
-	testCtrl.names = [];
+	testCtrl.geoCoords = [];
 	testCtrl.allOrgUnits = [];
 
 	// URL for orgunits-API
-	var apiUrl = "http://inf5750-14.uio.no/api/organisationUnits.json?pageSize=1332";
+	//var apiUrl = "http://inf5750-14.uio.no/api/organisationUnits.json?pageSize=1332";
+
+	// Tweaked URL to include coordinates (if applicable) for the facility.
+	// Not all facilities have coordinates. Figure out how to correctly represent those we can.
+	var apiUrl = "http://inf5750-14.uio.no/api/organisationUnits.json?fields=:identifiable,coordinates,level&pageSize=1332";
 
 	// Cross-site redirect error solution: Run chrome with --disable-web-security
 	var base64 = "YWRtaW46ZGlzdHJpY3Q=";
 	$http.get(apiUrl, {headers: {'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q='}}).
-		success(function(data) {
-			testCtrl.allOrgUnits = data.organisationUnits;
-			
-			// Filter limit i ng-repeat gjør at vi ikke trenger denne mer.
-			//testCtrl.orgUnits = data.organisationUnits.splice(0, 10);
-			
-			// Log stuff
-			console.log(data);
-			console.log(testCtrl.orgUnits);
-		}).
-		error(function(data, status, headers, config) {
-			alert("Error. Data: " + data);
-		});
+	success(function(data) {
+		testCtrl.allOrgUnits = data.organisationUnits;
 
+		// Log all the info
+		console.log(testCtrl.allOrgUnits);
 
+		// Reads all the coordinates. CURRENTLY ONLY GETS THOSE ON LEVEL 4.
+		// HARD-CODED AS FFFFFF. Creates pairs of [name, [coordinates]].
+		// Discuss how we implement this.
+		for (i = 0; i < testCtrl.allOrgUnits.length; i++)
+			if(testCtrl.allOrgUnits[i].coordinates != undefined && testCtrl.allOrgUnits[i].coordinates.length < 200)
+				testCtrl.geoCoords.push(new Array(testCtrl.allOrgUnits[i].name, testCtrl.allOrgUnits[i].coordinates.substring(1,testCtrl.allOrgUnits[i].coordinates.length-1).split(",")));
+
+		// Log all the coordinates.
+		//console.log(testCtrl.geoCoords);
+		
+		// Add the coordinates to the map.
+		addMarkers(testCtrl.geoCoords);
+
+	}).
+	error(function(data, status, headers, config) {
+		alert("Error. Data: " + data);
+	});
 }]);
---
+++ js/map.js
@@ -0,0 +1,51 @@
+//Implementation using Google Maps API.
+
+$(document).ready(initialize);
+	var map;
+
+// Initializes the map.
+function initialize() {
+
+	// Initial options. Centers on Sierra Leone.
+	var mapOptions = {
+			center: { lat: 8.4494988, lng: -11.7868289},
+			zoom: 8
+	};
+	// Creates the map.
+	map = new google.maps.Map(document.getElementById("map-canvas"),
+			mapOptions);
+}
+
+// Adds markers (from the coordinates array passed to this function) to the map.
+function addMarkers(coordinates) {
+
+	// Markers will be added dynamically.
+	var marker;
+
+	// InfoWindow to bind to the markers.
+	var infowindow = new google.maps.InfoWindow();
+	
+	// Custom image for the markers.
+	var customImage = 'https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png';
+
+	alert("We in business now, boys.");
+	
+	for (i = 0; i < coordinates.length; i++) { 
+		// Create and add a new marker per coordinate.
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(coordinates[i][1][1], coordinates[i][1][0]),
+			map: map,
+			icon: customImage,
+		});
+
+		// Add a listener to each marker, so that they will display the name of the facility when clicked.
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+				infowindow.setContent(coordinates[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+
+}
+google.maps.event.addDomListener(window, 'load', initialize);
